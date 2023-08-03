d8f5eaa54a67828097fa62e7588a908d773262d7
Tshwanelomosii <percymosii1@gmail.com>
2023-08-03 11:15:28 +0200

    updated navbar

--- podcast-app/src/components/Header.jsx
+++ podcast-app/src/components/Header.jsx
@@ -1,28 +1,46 @@
 import React from 'react';
-import { SlButton,SlInput } from '@shoelace-style/shoelace/dist/react';
+import { SlButton,SlInput,  SlDivider, SlDropdown, SlIcon, SlMenu, SlMenuItem } from '@shoelace-style/shoelace/dist/react';
 import './Header.css';
 
 
 const Header = () => {
   return (
-    <div className='navbar'>
-      <nav >
-        <ul>
-          <li><SlButton>Home</SlButton></li>
-          <li><SlButton>Discovery</SlButton></li>
-          <li><SlButton>favourites</SlButton></li>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"/></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
         </ul>
-      </nav>
-          <div className='searchButton'>
-            <SlInput  type='text'  placeholder='Search Podcast' width='30px'/>
-            <SlButton>Search</SlButton>
-          </div>
-      
-      
-
-      
-
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
     </div>
+  </nav>
   )
 }
 
--- podcast-app/src/main.jsx
+++ podcast-app/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '@shoelace-style/shoelace/dist/themes/light.styles.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
